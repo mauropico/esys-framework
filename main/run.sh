d91f8d9ffe33f05549ac7c8feb5671e66418a621
Mauro Picó <mauro@enia.io>
2024-05-30 10:03:36 -0300

    modifications for vtk

--- main/run.sh
+++ main/run.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
 # Execute script with ESySParticle.
-mpirun -np 2 esysparticle scripts/bingle_chk.py
+#mpirun -np 2 esysparticle scripts/bingle_chk.py
 
+# In progress:
 # Dump to VTK for Paraview Visualization
-#dump2vtk -i cpt -o frame -t 0 1001 1
+dump2vtk -i ./cpt -o ./frame -t 0 1000 100
 
