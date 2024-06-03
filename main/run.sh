8540aa827b61a974499f60243b04887af491500c
Mauro Picó <mauro@enia.io>
2024-06-03 17:03:21 -0300

    latest version with export paths

--- main/run.sh
+++ main/run.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+export PATH=/usr/local/bin/:$PATH
+export LIBRARY_PATH=/usr/local/lib/:$LIBRARY_PATH
+export LD_LIBRARY_PATH=/usr/local/lib/:$LD_LIBRARY_PATH
+export PYTHONPATH=/usr/local/lib/python3.10/dist-packages:/usr/local/lib/python3/dist-packages:$PYTHONPATH
 
 # Execute script with ESySParticle.
 mpirun -np 2 esysparticle scripts/bingle_chk.py
