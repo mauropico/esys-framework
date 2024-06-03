0a69b6408b75c16fd89da1f4591bb8026bc63bac
Mauro Picó <mauro@enia.io>
2024-06-03 11:52:21 -0300

    fixes for video tutorial

--- main/run.sh
+++ main/run.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 # Execute script with ESySParticle.
-#mpirun -np 2 esysparticle scripts/bingle_chk.py
+mpirun -np 2 esysparticle scripts/bingle_chk.py
 
 # In progress:
 # Dump to VTK for Paraview Visualization
-dump2vtk -i ./cpt -o ./frame -t 0 1000 100
+#dump2vtk -i ./cpt -o ./frame -t 0 1000 100
 
