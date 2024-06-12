e4352cf2c248633aaea9e0a4ac6f5958a04cde1a
Mauro Picó <mauro@enia.io>
2024-06-12 16:37:01 -0300

    framework ready to download

--- main/run.sh
+++ main/run.sh
@@ -7,13 +7,13 @@ export PYTHONPATH=/usr/local/lib/python3.10/dist-packages:/usr/local/lib/python3
 export PYTHONPATH="/usr/local/lib/python3/dist-packages/gengeo:${PYTHONPATH}"
 
 # Execute simulation script with ESyS-Particle.
-# mpirun -np 2 esysparticle scripts/bingle_chk.py
+mpirun -np 2 esysparticle scripts/bingle_chk.py
 
 # Dump to VTK results for Visualization with Paraview
 #dump2vtk -i ./cpt -o ./frame -t 0 1000 100
 
 # Generate geometry using GenGeo library. 
-python scripts/generate_geometry.py
+# python scripts/generate_geometry.py
 
 # To navigate in the container (similar to ssh into the machine)
 #bash
